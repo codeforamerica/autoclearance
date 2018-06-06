3ce6b7ee37d7b4f7e8d534b3dc0e8842f0cfa5da
Laura Kogler <lkogler@codeforamerica.org>
2018-06-06 10:53:54 -0700

    Add summary CSV file
    [#158135477]
    
    Signed-off-by: Paras Sanghavi <paras@codeforamerica.org>
    Co-authored-by: Paras Sanghavi <paras@codeforamerica.org>

--- app/helpers/rap_sheet_processor.rb
+++ app/helpers/rap_sheet_processor.rb
@@ -2,17 +2,34 @@ require 'csv'
 require 'rap_sheet_parser'
 
 class RapSheetProcessor
+  CSV_HEADER = [
+    'Date',
+    'Case Number',
+    'Courthouse',
+    'Charge',
+    'Severity',
+    'Sentence',
+    'Prop 64 Eligible',
+    'Needs info under 18',
+    'Needs info under 21',
+    'Needs info across state lines'
+  ]
+
   def self.run
     connection = Fog::Storage.new(Rails.configuration.fog_params)
     input_directory = connection.directories.new(key: Rails.configuration.input_bucket)
     output_directory = connection.directories.new(key: Rails.configuration.output_bucket)
 
+    summary_rows = []
+
     input_directory.files.each do |input_file|
       begin
         reader = PDF::Reader.new(StringIO.new(input_file.body))
+        rows = self.get_csv_rows(reader)
+        self.append_rows_to_summary(summary_rows, input_file.key, rows)
         output_directory.files.create(
           key: input_file.key.gsub('.pdf', '.csv'),
-          body: self.generate_output_file_contents(reader),
+          body: self.create_csv(CSV_HEADER, rows),
           content_type: 'text/csv'
         )
         input_file.destroy
@@ -22,6 +39,43 @@ class RapSheetProcessor
           body: e.message
         )
       end
+    end
+
+    timestamp = Time.now.strftime('%Y%m%d-%H%M%S')
+    output_directory.files.create(
+      key: "summary_#{timestamp}.csv",
+      body: self.create_csv(CSV_HEADER.unshift('Filename'), summary_rows),
+      content_type: 'text/csv'
+    )
+  end
+
+  def self.append_rows_to_summary(summary_rows, filename, rows)
+    rows.each do |row|
+      summary_rows << [filename] + row
+    end
+  end
+
+  def self.get_csv_rows(reader)
+    text = self.get_pdf_text(reader)
+    convictions = self.parse_convictions(text)
+    eligible_convictions = EligibilityChecker.new(convictions).eligible_events
+    self.csv_data(eligible_convictions.flat_map(&:counts))
+  end
+
+  def self.csv_data(counts)
+    counts.map do |count|
+      [
+        count.event.date,
+        count.event.case_number,
+        count.event.courthouse,
+        count.code_section,
+        count.severity,
+        count.event.sentence,
+        count.prop64_eligible?,
+        count.needs_info_under_18?,
+        count.needs_info_under_21?,
+        count.needs_info_across_state_lines?
+      ]
     end
   end
 
@@ -44,42 +98,12 @@ class RapSheetProcessor
     events.with_convictions
   end
 
-  def self.create_csv(conviction_counts)
+  def self.create_csv(header, rows)
     CSV.generate do |csv|
-      csv << [
-        'Date',
-        'Case Number',
-        'Courthouse',
-        'Charge',
-        'Severity',
-        'Sentence',
-        'Prop 64 Eligible',
-        'Needs info under 18',
-        'Needs info under 21',
-        'Needs info across state lines'
-      ]
-      conviction_counts.each do |count|
-        count_data = [
-          count.event.date,
-          count.event.case_number,
-          count.event.courthouse,
-          count.code_section,
-          count.severity,
-          count.event.sentence,
-          count.prop64_eligible?,
-          count.needs_info_under_18?,
-          count.needs_info_under_21?,
-          count.needs_info_across_state_lines?
-        ]
-        csv << count_data
+      csv << header
+      rows.each do |row|
+        csv << row
       end
     end
-  end
-
-  def self.generate_output_file_contents(reader)
-    text = self.get_pdf_text(reader)
-    convictions = self.parse_convictions(text)
-    eligible_convictions = EligibilityChecker.new(convictions).eligible_events
-    self.create_csv(eligible_convictions.flat_map(&:counts))
   end
 end
--- spec/helpers/rap_sheet_processor_spec.rb
+++ spec/helpers/rap_sheet_processor_spec.rb
@@ -14,16 +14,30 @@ describe RapSheetProcessor do
 
     it 'reads rap sheets from input bucket and saves conviction data to output bucket' do
       FileUtils.cp('spec/fixtures/skywalker_rap_sheet.pdf', '/tmp/autoclearance-rap-sheet-inputs/')
+      FileUtils.cp('spec/fixtures/chewbacca_rap_sheet.pdf', '/tmp/autoclearance-rap-sheet-inputs/')
 
-      described_class.run
+      travel_to Time.new(2010, 1, 2, 01, 04, 44) do
+        described_class.run
+      end
 
-      expect(Dir['/tmp/autoclearance-rap-sheet-inputs/skywalker_rap_sheet.pdf']).to eq []
-      expect(Dir['/tmp/autoclearance-outputs/*']).to eq ['/tmp/autoclearance-outputs/skywalker_rap_sheet.csv']
+      expect(Dir['/tmp/autoclearance-rap-sheet-inputs/*']).to eq []
+      expect(Dir['/tmp/autoclearance-outputs/*']).to contain_exactly(
+        '/tmp/autoclearance-outputs/skywalker_rap_sheet.csv',
+        '/tmp/autoclearance-outputs/chewbacca_rap_sheet.csv',
+        '/tmp/autoclearance-outputs/summary_20100102-010444.csv'
+      )
 
-      expected_text = File.read('spec/fixtures/skywalker_rap_sheet.csv')
-      actual_text = File.read('/tmp/autoclearance-outputs/skywalker_rap_sheet.csv')
+      skywalker_expected_text = File.read('spec/fixtures/skywalker_rap_sheet.csv')
+      skywalker_actual_text = File.read('/tmp/autoclearance-outputs/skywalker_rap_sheet.csv')
+      expect(skywalker_actual_text).to eq(skywalker_expected_text)
 
-      expect(actual_text).to eq(expected_text)
+      chewbacca_expected_text = File.read('spec/fixtures/chewbacca_rap_sheet.csv')
+      chewbacca_actual_text = File.read('/tmp/autoclearance-outputs/chewbacca_rap_sheet.csv')
+      expect(chewbacca_actual_text).to eq(chewbacca_expected_text)
+     
+      summary_expected_text = File.read('spec/fixtures/summary.csv')
+      summary_actual_text = File.read('/tmp/autoclearance-outputs/summary_20100102-010444.csv')
+      expect(summary_actual_text).to eq(summary_expected_text)
     end
 
     it 'catches exceptions in the parser and logs the error to an output file' do
@@ -40,11 +54,13 @@ describe RapSheetProcessor do
         end
       end
 
-      described_class.run
+      travel_to Time.new(2010, 1, 2, 01, 04, 44) do
+        described_class.run
+      end
 
       expect(Dir['/tmp/autoclearance-rap-sheet-inputs/*']).to eq ['/tmp/autoclearance-rap-sheet-inputs/not_a_valid_rap_sheet.pdf']
-      expect(Dir['/tmp/autoclearance-outputs/*']).to eq ['/tmp/autoclearance-outputs/not_a_valid_rap_sheet.error',
-                                                         '/tmp/autoclearance-outputs/skywalker_rap_sheet.csv']
+      expect(Dir['/tmp/autoclearance-outputs/*']).to contain_exactly('/tmp/autoclearance-outputs/not_a_valid_rap_sheet.error',
+        '/tmp/autoclearance-outputs/skywalker_rap_sheet.csv', '/tmp/autoclearance-outputs/summary_20100102-010444.csv')
 
       actual_text = File.read('/tmp/autoclearance-outputs/not_a_valid_rap_sheet.error')
 
@@ -66,19 +82,19 @@ describe RapSheetProcessor do
   describe '.strip_page_header_and_footer' do
     let :expected_text do
       expected_text = <<~EXPECTED_TEXT
-      document begin here
-      another line
+        document begin here
+        another line
       EXPECTED_TEXT
     end
 
     it 'removes blank lines and whitespace at the beginning of page' do
       input_text = <<~INPUT_TEXT
-      
-          
-            \t
-      
-      document begin here
-      another line
+
+            
+              \t
+
+        document begin here
+        another line
       INPUT_TEXT
 
       expect(described_class.strip_page_header_and_footer(input_text)).to eq expected_text
@@ -86,11 +102,11 @@ describe RapSheetProcessor do
 
     it 'removes blank lines and whitespace at the end of page' do
       input_text = <<~INPUT_TEXT
-      document begin here
-      another line
-    
-          
-            \t
+        document begin here
+        another line
+
+            
+              \t
 
       INPUT_TEXT
 
@@ -100,10 +116,10 @@ describe RapSheetProcessor do
     it 'removes lines beginning with "Page" from beginning of page' do
       input_text = <<~INPUT_TEXT
 
-                Page 1 of 5
+                  Page 1 of 5
 
-      document begin here
-      another line
+        document begin here
+        another line
       INPUT_TEXT
 
       expect(described_class.strip_page_header_and_footer(input_text)).to eq expected_text
@@ -112,11 +128,11 @@ describe RapSheetProcessor do
     it 'removes lines beginning with "Route" from beginning of page' do
       input_text = <<~INPUT_TEXT
 
-                
-        Route  Print
+                  
+          Route  Print
 
-      document begin here
-      another line
+        document begin here
+        another line
       INPUT_TEXT
 
       expect(described_class.strip_page_header_and_footer(input_text)).to eq expected_text
@@ -124,9 +140,9 @@ describe RapSheetProcessor do
 
     it 'removes lines beginning with "http://" from end of page' do
       input_text = <<~INPUT_TEXT
-      document begin here
-      another line
-      http://example.com
+        document begin here
+        another line
+        http://example.com
 
       INPUT_TEXT
 
@@ -135,11 +151,11 @@ describe RapSheetProcessor do
 
     it 'removes lines beginning with "https://" from end of page' do
       input_text = <<~INPUT_TEXT
-      document begin here
-      another line
+        document begin here
+        another line
 
 
-           https://example.com   5/16/1997
+             https://example.com   5/16/1997
       INPUT_TEXT
 
       expect(described_class.strip_page_header_and_footer(input_text)).to eq expected_text
@@ -147,11 +163,11 @@ describe RapSheetProcessor do
 
     it 'removes lines beginning with "file://" or "ﬁle://" from end of page' do
       input_text = <<~INPUT_TEXT
-      document begin here
-      another line
-           ﬁle:///example.com   5/16/1997
+        document begin here
+        another line
+             ﬁle:///example.com   5/16/1997
 
-      file:///path/to/file
+        file:///path/to/file
 
       INPUT_TEXT
 
