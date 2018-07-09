673461a9b5a0cb5535901e187a05a465dd5eb18e
Paras Sanghavi <paras@codeforamerica.org>
2018-07-09 16:54:03 -0700

    Render 404 for non-existant routes
    [#158811126]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def not_found
+    render :file => 'public/404.html', :status => :not_found, :layout => false
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   get '/', to: proc { [200, {}, ['']] }
   
   resources :jobs, only: [:index, :create]
+
+  # keep last
+  match '*path', via: [:all], to: 'application#not_found'
 end
