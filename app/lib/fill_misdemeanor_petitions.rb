5e8e9e9a9147b5c482cc42b34999a8f9e3cb60a5
Symonne Singleton <symonne@codeforamerica.org>
2018-08-07 14:33:40 -0700

    Use new misdemeanor motion provided from the DA
    [#159513352]
    
    Signed-off-by: Paras Sanghavi <paras@codeforamerica.org>
    Co-authored-by: Paras Sanghavi <paras@codeforamerica.org>

--- app/lib/fill_misdemeanor_petitions.rb
+++ app/lib/fill_misdemeanor_petitions.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 class FillMisdemeanorPetitions
   def initialize(path:, output_dir:)
     @path = path
@@ -7,58 +9,62 @@ class FillMisdemeanorPetitions
   def fill
     # Row
     # 0 Courtno
-    # 1 True Name
-    # 2 DOB
-    # 3 Conviction Date
-    # 4 Sentence Date
-    # 5 Case Type  
-    # 6 Dispo Code
-    # 7 Disposition                       
-    # 8 11357(a)
-    # 9 11357(b)
-    # 10 11357(c)
-    # 11 11359
-    # 12 11359(a)
-    # 13 11359(b)
-    # 14 11360(a)
-    # 15 11360(b)
-    # 16 Other
-    # 17 Petition Created
-    # 18 Date Petition Drafted
-    # 19 Date Petition Submitted
-    # 20 Petition Granted
-    # 21 Date Sent to PDR
-    # 22 Notes
-    # 23 Copy Requested
-    # 24 Email Address
-    # 25 Mailing Address
-    # 26 Willing to Speak to Media
-    # 27 Phone Number
+    # 1 SFNO
+    # 2 True Name
+    # 3 DOB
+    # 4 Conviction Date
+    # 5 Sentence Date
+    # 6 Case Type
+    # 7 Dispo Code
+    # 8 Disposition
+    # 9 11357(a)
+    # 10 11357(b)
+    # 11 11357(c)
+    # 12 11359
+    # 13 11359(a)
+    # 14 11359(b)
+    # 15 11360(a)
+    # 16 11360(b)
+    # 17 Other
+    # 18 Petition Created
+    # 19 Date Petition Drafted
+    # 20 Date Petition Submitted
+    # 21 Petition Granted
+    # 22 Date Sent to PDR
+    # 23 Notes
+    # 24 Copy Requested
+    # 25 Email Address
+    # 26 Mailing Address
+    # 27 Willing to Speak to Media
+    # 28 Phone Number
 
-    CSV.foreach(path, headers: true) do |row|
-      case_number = row[0].strip
-      name = row[1].strip
-      date_of_birth = row[2].strip
-      count_11357a = row[8].strip
-      count_11357b = row[9].strip
-      count_11357c = row[10].strip
-      count_11359 = row[11].strip
+    Dir.mkdir("#{output_dir}/with_sf_number") unless File.exists?("#{output_dir}/with_sf_number")
+    Dir.mkdir("#{output_dir}/no_sf_number") unless File.exists?("#{output_dir}/no_sf_number")
+
+    CSV.foreach(path, headers: true, encoding: 'ISO-8859-1') do |row|
+      court_number = row[0].strip
+      name = row[2].strip
+      date_of_birth = row[3].strip
+      count_11357a = row[9].strip
+      count_11357b = row[10].strip
+      count_11357c = row[11].strip
 
       other_charges = others(row)
 
       fields = {
-        "last name, first name, middle name (DOB)": "#{name} (DOB #{date_of_birth})",
-        "Court Number": case_number,
-        "Check Box2": checkbox_val(count_11357a),
-        "Check Box3": checkbox_val(count_11357b),
-        "Check Box4": checkbox_val(count_11357c),
-        "Check Box5": checkbox_val(count_11359),
-        "Check Box7": yes(other_charges.present?),
-        "PROSECUTING ATTORNEY REQUEST FOR DISMISSAL AND SEALING": other_charges.join(', '),
-        "Date": Date.today
+        "defendant": "#{name} (DOB #{date_of_birth})",
+        "sf_number": sf_number(row),
+        "court_number": court_number,
+        "11357a": checkbox_val(count_11357a),
+        "11357b": checkbox_val(count_11357b),
+        "11357c": checkbox_val(count_11357c),
+        "11360b": "No", # ignored right now since column always empty
+        "others": yes(other_charges.present?),
+        "other_charges_description": other_charges.join(', '),
+        "date": Date.today
       }
 
-      fill_petition(name.gsub('/', '_'), fields)
+      fill_petition("#{name.gsub('/', '_')}_#{court_number}", sf_number(row), fields)
     end
   end
 
@@ -66,28 +72,35 @@ class FillMisdemeanorPetitions
 
   attr_reader :path, :output_dir
 
+  def sf_number(row)
+    value = row[1].strip
+
+    value unless value == "0"
+  end
+
   def others(row)
-    count_11359a = row[12]&.strip
-    count_11359b = row[13]&.strip
-    count_11360a = row[14]&.strip
-    
-    [count_11359a, count_11359b, count_11360a].compact.reject do |data|
+    count_11359 = row[12]&.strip
+    count_11359a = row[13]&.strip
+    count_11359b = row[14]&.strip
+    count_11360a = row[15]&.strip
+
+    [count_11359, count_11359a, count_11359b, count_11360a].compact.reject do |data|
       data.empty? || data.include?('#N/A')
     end
   end
 
   def yes(bool)
-    "Yes" if bool
+    bool ? "Yes" : "No"
   end
 
   def checkbox_val(data)
-    'Yes' unless data.include?('#N/A')
+    data.include?('#N/A') ? "No" : "Yes"
   end
 
-  def fill_petition(name, fields)
-    file_name = "#{output_dir}/petition_#{name}.pdf"
+  def fill_petition(name, sf_number, fields)
+    sub_directory = sf_number ? "with_sf_number" : "no_sf_number"
+    file_name = "#{output_dir}/#{sub_directory}/petition_#{name}.pdf"
     pdftk = PdfForms.new(Cliver.detect('pdftk'))
-
     pdftk.fill_form 'app/assets/petitions/prop64_misdemeanors.pdf', file_name, fields
   end
 end
--- spec/lib/fill_misdemeanor_petitions_spec.rb
+++ spec/lib/fill_misdemeanor_petitions_spec.rb
@@ -7,8 +7,8 @@ describe FillMisdemeanorPetitions do
   it 'populates motions per row' do
     t = Tempfile.new("test_csv")
     t << "some_headers\n"
-    t << "123,SMITH/AGENT   ,1/1/99,,,,,,  #N/A , #N/A ,11357(c), #N/A, , , , ,  \n"
-    t << "123,CANDY/AGENT   ,1/1/99,,,,,,  11357(a) , #N/A , #N/A, #N/A , 11359(a), , , ,   \n"
+    t << "123, 010101, SMITH/AGENT   ,1/1/99,,,,,,  #N/A , #N/A ,11357©, #N/A, , , , #N/A, ,   \n"
+    t << "456, 293848, CANDY/AGENT   ,1/1/99,,,,,,  11357(a) , #N/A , #N/A, #N/A , 11359(a), #N/A,11360(a),,   \n"
     t.close
 
     output_dir = Dir.mktmpdir
@@ -17,28 +17,64 @@ describe FillMisdemeanorPetitions do
       described_class.new(path: t, output_dir: output_dir).fill
     end
 
-    expect(Dir["#{output_dir}/*"].length).to eq 2
+    expect(Dir["#{output_dir}/**/*.pdf"].length).to eq 2
 
-    fields = get_fields_from_pdf(File.new("#{output_dir}/petition_SMITH_AGENT.pdf"))
-    expect(fields["last name, first name, middle name (DOB)"]).to eq "SMITH/AGENT (DOB 1/1/99)"
-    expect(fields["Court Number"]).to eq "123"
-    expect(fields["Check Box2"]).to eq ""
-    expect(fields["Check Box3"]).to eq ""
-    expect(fields["Check Box4"]).to eq "Yes"
-    expect(fields["Check Box5"]).to eq ""
-    expect(fields["Check Box7"]).to eq ""
-    expect(fields["PROSECUTING ATTORNEY REQUEST FOR DISMISSAL AND SEALING"]).to eq ""
-    expect(fields["Date"]).to eq "1999-10-01"
+    fields = get_fields_from_pdf(File.new("#{output_dir}/with_sf_number/petition_SMITH_AGENT_123.pdf"))
 
-    fields = get_fields_from_pdf(File.new("#{output_dir}/petition_CANDY_AGENT.pdf"))
-    expect(fields["last name, first name, middle name (DOB)"]).to eq "CANDY/AGENT (DOB 1/1/99)"
-    expect(fields["Court Number"]).to eq "123"
-    expect(fields["Check Box2"]).to eq "Yes"
-    expect(fields["Check Box3"]).to eq ""
-    expect(fields["Check Box4"]).to eq ""
-    expect(fields["Check Box5"]).to eq ""
-    expect(fields["Check Box7"]).to eq "Yes"
-    expect(fields["PROSECUTING ATTORNEY REQUEST FOR DISMISSAL AND SEALING"]).to eq "11359(a)"
-    expect(fields["Date"]).to eq "1999-10-01"
+    expect(fields).to eq({
+      "defendant" => "SMITH/AGENT (DOB 1/1/99)",
+      "sf_number" => "010101",
+      "court_number" => "123",
+      "11357a" => "No",
+      "11357b" => "No",
+      "11357c" => "Yes",
+      "11360b" => "No",
+      "others" => "No",
+      "other_charges_description" => "",
+      "date" => "1999-10-01",
+      "completed_sentence" => "Yes",
+      "court_dismisses" => "Yes",
+      "court_granted" => "Yes",
+      "court_relieved" => "Yes",
+      "court_sealed" => "Yes",
+      "record_sealed" => "Yes",
+      "dismissal" => "Yes"
+    })
+
+    fields = get_fields_from_pdf(File.new("#{output_dir}/with_sf_number/petition_CANDY_AGENT_456.pdf"))
+    expect(fields).to eq({
+      "defendant" => "CANDY/AGENT (DOB 1/1/99)",
+      "sf_number" => "293848",
+      "court_number" => "456",
+      "11357a" => "Yes",
+      "11357b" => "No",
+      "11357c" => "No",
+      "11360b" => "No",
+      "others" => "Yes",
+      "other_charges_description" => "11359(a), 11360(a)",
+      "date" => "1999-10-01",
+      "completed_sentence" => "Yes",
+      "court_dismisses" => "Yes",
+      "court_granted" => "Yes",
+      "court_relieved" => "Yes",
+      "court_sealed" => "Yes",
+      "record_sealed" => "Yes",
+      "dismissal" => "Yes"
+    })
+  end
+
+  it 'does not fill in sf number if it is zero' do
+    t = Tempfile.new("test_csv")
+    t << "some_headers\n"
+    t << "123, 0 , SMITH/AGENT   ,1/1/99,,,,,,  #N/A , #N/A ,11357(c), #N/A, , , , #N/A, ,   \n"
+    t.close
+
+    output_dir = Dir.mktmpdir
+    described_class.new(path: t, output_dir: output_dir).fill
+    expect(Dir["#{output_dir}/**/*.pdf"].length).to eq 1
+
+    fields = get_fields_from_pdf(File.new("#{output_dir}/no_sf_number/petition_SMITH_AGENT_123.pdf"))
+
+    expect(fields["sf_number"]).to eq ''
   end
 end
