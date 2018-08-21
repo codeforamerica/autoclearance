59a7813801c8394ef82dbba87c7ed15c4476b788
Laura Kogler <lkogler@codeforamerica.org>
2018-08-20 17:25:15 -0700

    Remove unneeded logic in AnonCycle to filter by event type
    [#159898510]

--- app/models/anon_cycle.rb
+++ app/models/anon_cycle.rb
@@ -4,18 +4,9 @@ class AnonCycle < ApplicationRecord
 
   def self.build_from_parser(cycle)
     events = cycle.events.map do |event|
-      if allowed_event?(event)
-        AnonEvent.build_from_parser(event)
-      end
-    end.compact
-    
+      AnonEvent.build_from_parser(event)
+    end
+
     AnonCycle.new(anon_events: events)
-  end
-
-  private
-
-  def self.allowed_event?(event)
-    event.is_a?(RapSheetParser::CourtEvent) ||
-    (event.is_a?(RapSheetParser::OtherEvent) && ['arrest','applicant', 'probation', 'custody', 'registration'].include?(event.header))
   end
 end
