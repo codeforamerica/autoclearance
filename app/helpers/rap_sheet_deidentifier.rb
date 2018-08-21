fdcfbcabf0f169082a4348c7ed85698bd8d5679b
Symonne Singleton <symonne@codeforamerica.org>
2018-08-21 13:46:46 -0700

    Co-authored-by: Paras Sanghavi <paras@codeforamerica.org>

--- app/helpers/rap_sheet_deidentifier.rb
+++ app/helpers/rap_sheet_deidentifier.rb
@@ -8,13 +8,33 @@ class RapSheetDeidentifier
   def run
     @input_directory.files.to_a.sort_by { |f| f.key }.each do |input_file|
       text = PDFReader.new(input_file.body).text
+      next unless first_cycle_delimiter_index(text)
+
       id = Digest::MD5.hexdigest(text)
       deidentified_text = deidentify(text)
       @output_directory.files.create(
         key: "redacted_rap_sheet_#{id}.txt",
         body: deidentified_text
       )
+
+      @output_directory.files.create(
+        key: "redacted_rap_sheet_#{id}.pdf",
+        body: pdf_contents(deidentified_text)
+      )
     end
+  end
+
+  private
+
+  def pdf_contents(text)
+    tmp_text = Tempfile.new
+    tmp_text.write(text)
+    tmp_text.close
+
+    tmp_pdf = Tempfile.new
+    `enscript -B #{tmp_text.path} -q -o - | ps2pdf -q - #{tmp_pdf.path}`
+
+    tmp_pdf.read
   end
 
   def deidentify(text)
@@ -57,8 +77,41 @@ class RapSheetDeidentifier
   end
 
   def remove_personal_info_section(text)
-    slice_index = text.index(/^\s*(\*\s?){4}$/)
-    text.slice!(0..(slice_index - 1))
-    text
+    text.slice!(0..(first_cycle_delimiter_index(text) - 1))
+    text.prepend(fake_personal_info)
   end
+
+  def first_cycle_delimiter_index(text)
+    text.index(/^\s*(\*\s?){4}$/)
+  end
+end
+
+def fake_personal_info
+  <<~TEXT
+    FROM: CLET   ISN: 00003 DATE: 02/05/01 TIME: 09:36:27 RESP MSG
+    TO:   CT032474  OSN: 00003 DATE: 05/15/12 TIME: 09:36:30
+    USERID: OBI_WAN
+    
+    IH
+    
+    RE: QHY.CA0313113A.10243565.PM,PM,P      DATE:20010205 TIME:09:36:26
+    RESTRICTED-DO NOT USE FOR EMPLOYMENT,LICENSING OR CERTIFICATION PURPOSES
+    ATTN:PM,PM,P,O,1954249,
+
+    ************************************************************************
+    FOR CALIFORNIA AGENCIES ONLY - HAS PREVIOUS QUALIFYING OFFENSE. COLLECT DNA IF INCARCERATED, CONFINED, OR ON PROBATION OR PAROLE FOLLOWING ANY MISDEMEANOR OR FELONY CONVICTION. REQUEST KITS AND INFO AT (510) 310- 3000 OR PC296.PC296@DOJ.CA.GOV.
+    ************************************************************************
+    
+    ** III MULTIPLE SOURCE RECORD
+    
+    CII/A01234557
+    DOB/19681122    SEX/M  RAC/WHITE
+    HGT/511  WGT/265  EYE/BLU  HAI/BRO  POB/CA
+    NAM/01 LAST, FIRST
+    02 NAME, BOB
+    FBI/7778889LA2
+    CDL/C45667234 C14546456
+    SOC/549377146
+    OCC/CONCRET
+  TEXT
 end
--- spec/helpers/rap_sheet_deidentifier_spec.rb
+++ spec/helpers/rap_sheet_deidentifier_spec.rb
@@ -18,10 +18,11 @@ describe RapSheetDeidentifier do
       described_class.new.run
 
       expect(Dir['/tmp/autoclearance-rap-sheet-inputs/*'].length).to eq 1
-      output_files = Dir['/tmp/autoclearance-outputs/redacted_rap_sheet_*']
-      expect(output_files.length).to eq 1
+      redacted_text = Dir['/tmp/autoclearance-outputs/redacted_rap_sheet_*.txt']
+      expect(redacted_text.length).to eq 1
 
-      skywalker_actual_text = File.read(output_files[0])
+      skywalker_actual_text = File.read(redacted_text[0])
+      expect(skywalker_actual_text).to include('496 PC-RECEIVE/ETC KNOWN STOLEN PROPERTY')
       expect(skywalker_actual_text).not_to include('SKYWALKER')
       expect(skywalker_actual_text).not_to include('LUKE')
       expect(skywalker_actual_text).not_to include('JAY')
@@ -33,6 +34,34 @@ describe RapSheetDeidentifier do
       expect(skywalker_actual_text).not_to include('#2222222')
       expect(skywalker_actual_text).not_to include('#3456789')
       expect(skywalker_actual_text).not_to include('#33857493')
+
+      redacted_pdf = Dir['/tmp/autoclearance-outputs/redacted_rap_sheet_*.pdf']
+      expect(redacted_pdf.length).to eq 1
+
+      pdf_text = PDFReader.new(File.read(redacted_pdf[0])).text
+      expect(pdf_text).to include('CII/A01234557')
+      expect(pdf_text).to include('496 PC-RECEIVE/ETC KNOWN STOLEN PROPERTY')
+      expect(pdf_text).not_to include('SKYWALKER')
+      expect(pdf_text).not_to include('LUKE')
+      expect(pdf_text).not_to include('JAY')
+      expect(pdf_text).not_to include('A99000099')
+      expect(pdf_text).not_to include('19660119')
+      expect(pdf_text).not_to include('19550505')
+      expect(pdf_text).not_to include('#1111111')
+      expect(pdf_text).not_to include('#1234567')
+      expect(pdf_text).not_to include('#2222222')
+      expect(pdf_text).not_to include('#3456789')
+      expect(pdf_text).not_to include('#33857493')
+    end
+
+    it 'skip rap sheets without any cycles' do
+      File.open('/tmp/autoclearance-rap-sheet-inputs/bad_rap_sheet.pdf', 'w') {|f| f.write('Personal info') }
+
+      allow_any_instance_of(PDFReader).to receive(:text).and_return('Personal info')
+
+      described_class.new.run
+
+      expect(Dir['/tmp/autoclearance-outputs/*'].length).to eq 0
     end
   end
 end
