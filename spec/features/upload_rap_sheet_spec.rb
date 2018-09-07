0db3d61ccbf42547c14f17c5b6e01ff195bc2253
Zak Auerbach <zak@codeforamerica.org>
2018-09-07 13:54:05 -0700

    wip page for viewing uploaded files

--- spec/features/upload_rap_sheet_spec.rb
+++ spec/features/upload_rap_sheet_spec.rb
@@ -4,5 +4,7 @@ feature 'uploading rap sheets' do
   scenario 'user uploads a few rap sheets' do
     visit root_path
     expect(page).to have_content 'Upload RAP sheets'
+
+    attach_file 'Select files', Rails.root.join('spec', 'fixtures', 'chewbacca_rap_sheet.pdf')
   end
 end
