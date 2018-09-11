f683130b85ee3e4ead86809e3ee415504c5764c8
Laura Kogler <lkogler@codeforamerica.org>
2018-09-10 17:36:56 -0700

    Enable "Upload" button once files are selected
    * add styling to list items, including filename truncation at 25 characters
    * add "yarn install" step to circle ci config
    
    [#15990006]
    
    Co-authored-by: Symonne Singleton <symonne@codeforamerica.org>

--- spec/features/upload_rap_sheet_spec.rb
+++ spec/features/upload_rap_sheet_spec.rb
@@ -4,6 +4,7 @@ feature 'uploading rap sheets' do
   scenario 'user uploads a few rap sheets', js: true do
     visit root_path
     expect(page).to have_content 'Upload RAP sheets'
+    expect(page).to have_button('Upload', disabled: true)
 
     files = [Rails.root.join('spec', 'fixtures', 'chewbacca_rap_sheet.pdf'), Rails.root.join('spec', 'fixtures', 'skywalker_rap_sheet.pdf')]
 
@@ -11,5 +12,7 @@ feature 'uploading rap sheets' do
 
     expect(page).to have_content 'chewbacca_rap_sheet.pdf'
     expect(page).to have_content 'skywalker_rap_sheet.pdf'
+
+    expect(page).to have_button('Upload', disabled: false)
   end
 end
