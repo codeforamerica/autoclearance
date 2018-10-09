711f7f3b792d2f685d61a5e16bc16f7e827764a3
Paras Sanghavi <paras@codeforamerica.org>
2018-10-09 15:00:26 -0700

    fix bastion_setup script with correct path

--- scripts/bastion_setup.sh
+++ scripts/bastion_setup.sh
@@ -3,10 +3,10 @@ set -e
 
 IP=$1
 
-scp adduser.sh ec2-user@${IP}:
+scp scripts/adduser.sh ec2-user@${IP}:
 ssh ec2-user@${IP} -C "chmod a+x ./adduser.sh && sudo ./adduser.sh"
 
 ssh ec2-user@${IP} -C "sudo yum update -y && sudo yum install -y awslogs"
-scp bastion_awslogs.conf ec2-user@${IP}:awslogs.conf
-scp bastion_awscli.conf ec2-user@${IP}:awscli.conf
+scp scripts/bastion_awslogs.conf ec2-user@${IP}:awslogs.conf
+scp scripts/bastion_awscli.conf ec2-user@${IP}:awscli.conf
 ssh ec2-user@${IP} -C "sudo mv awslogs.conf /etc/awslogs/awslogs.conf && sudo mv awscli.conf /etc/awslogs/awscli.conf && sudo service awslogs restart"
