9e0112610da612f8559c66b38fe7836c33f55c61
Paras Sanghavi <paras@codeforamerica.org>
2018-11-09 10:59:52 -0800

    Revert "update name of env var for awsebcli"
    This reverts commit 5edb1a980f142f9aed06ae36b2e778248a68178c.
    
    Co-authored-by: Laura Kogler <lkogler@codeforamerica.org>

--- scripts/setup_eb.sh
+++ scripts/setup_eb.sh
@@ -7,4 +7,4 @@ touch ~/.aws/config
 chmod 600 ~/.aws/config
 echo "[profile autoclearance]" > ~/.aws/config
 echo "aws_access_key_id=$AWS_ACCESS_KEY_ID" >> ~/.aws/config
-echo "aws_secret_access_key=$AWS_SECRET_ACCESS_KEY" >> ~/.aws/config
+echo "aws_secret_access_key=$AWS_SECRET_KEY" >> ~/.aws/config
