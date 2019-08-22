77df8fb1877fa79fae3b7d213614d2a79f8ae414
Christa Hartsock <christa@codeforamerica.org>
2019-08-22 14:49:25 -0700

    Modify bastion restart script to accept identity file

--- scripts/bastion_setup.sh
+++ scripts/bastion_setup.sh
@@ -2,11 +2,12 @@
 set -e
 
 IP=$1
+SSH_KEY=$2
 
-scp scripts/adduser.sh ec2-user@${IP}:
-ssh ec2-user@${IP} -C "chmod a+x ./adduser.sh && sudo ./adduser.sh"
+scp  -i "${SSH_KEY}" scripts/adduser.sh ec2-user@${IP}:
+ssh ec2-user@${IP} -i "${SSH_KEY}" -C "chmod a+x ./adduser.sh && sudo ./adduser.sh"
 
-ssh ec2-user@${IP} -C "sudo yum update -y && sudo yum install -y awslogs"
-scp scripts/bastion_awslogs.conf ec2-user@${IP}:awslogs.conf
-scp scripts/bastion_awscli.conf ec2-user@${IP}:awscli.conf
-ssh ec2-user@${IP} -C "sudo mv awslogs.conf /etc/awslogs/awslogs.conf && sudo mv awscli.conf /etc/awslogs/awscli.conf && sudo service awslogs restart"
+ssh ec2-user@${IP} -i "${SSH_KEY}" -C "sudo yum update -y && sudo yum install -y awslogs"
+scp -i "${SSH_KEY}" scripts/bastion_awslogs.conf ec2-user@${IP}:awslogs.conf
+scp -i "${SSH_KEY}" scripts/bastion_awscli.conf ec2-user@${IP}:awscli.conf
+ssh ec2-user@${IP} -i "${SSH_KEY}" -C "sudo mv awslogs.conf /etc/awslogs/awslogs.conf && sudo mv awscli.conf /etc/awslogs/awscli.conf && sudo service awslogs restart"
