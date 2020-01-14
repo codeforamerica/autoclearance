d546a705d2e44bfb7fc683bed1d9b24be50fa5e0
Molly Trombley-McCann <mollyt@codeforamerica.org>
2020-01-14 14:22:23 -0800

    Terraform: Remove unused S3 buckets
    Signed-off-by: Christa Hartsock <christa@codeforamerica.org>
    Co-authored-by: Christa Hartsock <christa@codeforamerica.org>

--- terraform/main/main.tf
+++ terraform/main/main.tf
@@ -1,170 +1,5 @@
-resource "aws_kms_key" "k" {
-  description = "autoclearance"
-}
-
-resource "aws_kms_alias" "k" {
-  name = "alias/autoclearance"
-  target_key_id = "${aws_kms_key.k.key_id}"
-}
-
-resource "aws_s3_bucket" "rap_sheet_inputs" {
-  bucket = "autoclearance-rap-sheet-inputs-${var.environment}"
-
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        kms_master_key_id = "${aws_kms_key.k.arn}"
-        sse_algorithm = "aws:kms"
-      }
-    }
-  }
-
-  policy = <<POLICY
-{
-    "Version": "2008-10-17",
-    "Id": "Policy5",
-    "Statement": [
-        {
-            "Sid": "DenyUnSecureCommunications",
-            "Effect": "Deny",
-            "Principal": {
-                "AWS": "*"
-            },
-            "Action": "s3:*",
-            "Resource": "arn:aws-us-gov:s3:::autoclearance-rap-sheet-inputs-${var.environment}/*",
-            "Condition": {
-                "Bool": {
-                    "aws:SecureTransport": "false"
-                }
-            }
-        }
-    ]
-}
-POLICY
-}
-
-resource "aws_s3_bucket" "autoclearance_outputs" {
-  bucket = "autoclearance-outputs-${var.environment}"
-
-
-  versioning {
-    enabled = true
-  }
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        kms_master_key_id = "${aws_kms_key.k.arn}"
-        sse_algorithm = "aws:kms"
-      }
-    }
-  }
-
-  policy = <<POLICY
-{
-    "Version": "2008-10-17",
-    "Id": "Policy-GENERATED-ID",
-    "Statement": [
-        {
-            "Sid": "DenyUnSecureCommunications",
-            "Effect": "Deny",
-            "Principal": {
-                "AWS": "*"
-            },
-            "Action": "s3:*",
-            "Resource": "arn:aws-us-gov:s3:::autoclearance-outputs-${var.environment}/*",
-            "Condition": {
-                "Bool": {
-                    "aws:SecureTransport": "false"
-                }
-            }
-        }
-    ]
-}
-POLICY
-}
-
-resource "aws_iam_policy" "s3_read_write" {
-  name = "s3_read_write"
-  description = "Interact with S3 for application inputs and outputs"
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "s3:DeleteObject",
-        "s3:PutObject",
-        "s3:GetObject",
-        "s3:ListBucket",
-        "s3:GetObjectAcl",
-        "s3:GetBucketPolicy",
-        "s3:GetBucketAcl"
-      ],
-      "Effect": "Allow",
-      "Resource": [
-          "${aws_s3_bucket.autoclearance_outputs.arn}",
-          "${aws_s3_bucket.rap_sheet_inputs.arn}",
-          "${aws_s3_bucket.autoclearance_outputs.arn}/*",
-          "${aws_s3_bucket.rap_sheet_inputs.arn}/*"
-      ]
-    },
-    {
-      "Effect": "Allow",
-      "Action": "s3:ListAllMyBuckets",
-      "Resource": "*"
-    },
-    {
-      "Sid": "KMSPermissionsForAutoclearanceKey",
-      "Effect": "Allow",
-      "Action": [
-        "kms:Decrypt",
-        "kms:ListKeyPolicies",
-        "kms:ListRetirableGrants",
-        "kms:GetKeyPolicy",
-        "kms:GenerateDataKeyWithoutPlaintext",
-        "kms:ListResourceTags",
-        "kms:ListGrants",
-        "kms:GetParametersForImport",
-        "kms:Encrypt",
-        "kms:GetKeyRotationStatus",
-        "kms:DescribeKey"
-      ],
-      "Resource": [
-        "${aws_kms_key.k.arn}"
-      ]
-    },
-    {
-      "Sid": "GlobalKMSPermissions",
-      "Effect": "Allow",
-      "Action": [
-        "kms:ListKeys",
-        "kms:GenerateRandom",
-        "kms:ListAliases",
-        "kms:ReEncryptTo",
-        "kms:GenerateDataKey",
-        "kms:ReEncryptFrom"
-      ],
-      "Resource": "*"
-    }
-  ]
-}
-EOF
-}
-
 resource "aws_iam_group" "staff" {
   name = "staff"
-}
-
-resource "aws_iam_group_policy_attachment" "s3_read_write_for_staff" {
-  group = "${aws_iam_group.staff.name}"
-  policy_arn = "${aws_iam_policy.s3_read_write.arn}"
-}
-
-resource "aws_key_pair" "auth" {
-  key_name = "${var.key_name}"
-  public_key = "${var.public_key}"
 }
 
 resource "aws_config_config_rule" "r" {
@@ -219,6 +54,8 @@ resource "aws_iam_role_policy_attachment" "a" {
   policy_arn = "arn:aws-us-gov:iam::aws:policy/service-role/AWSConfigRole"
 }
 
+// Bucket for logs for changes to AWS account configuration
+// Retain until account decommissioned
 resource "aws_s3_bucket" "config" {
   bucket = "awsconfig-${var.environment}"
   force_destroy = true
@@ -363,62 +200,8 @@ resource "aws_iam_policy" "cloudwatch_s3_policy" {
 POLICY
 }
 
-resource "aws_cloudtrail" "s3_logs" {
-  name = "s3-logs"
-  s3_bucket_name = "${aws_s3_bucket.cloudtrail_s3_logs.id}"
-  include_global_service_events = false
-  enable_log_file_validation = true
-
-  cloud_watch_logs_group_arn = "${aws_cloudwatch_log_group.s3_logs.arn}"
-  cloud_watch_logs_role_arn = "${aws_iam_role.cloudwatch_logs_role.arn}"
-
-  event_selector {
-    read_write_type = "All"
-    include_management_events = false
-
-    data_resource {
-      type = "AWS::S3::Object"
-      values = [
-        "${aws_s3_bucket.autoclearance_outputs.arn}/",
-        "${aws_s3_bucket.rap_sheet_inputs.arn}/"
-      ]
-    }
-  }
-}
-
-resource "aws_cloudtrail" "log_access_logs" {
-  name = "log-access-logs"
-  s3_bucket_name = "${aws_s3_bucket.cloudtrail_log_access_logs.id}"
-  include_global_service_events = false
-  enable_log_file_validation = true
-
-  cloud_watch_logs_group_arn = "${aws_cloudwatch_log_group.log_access_logs.arn}"
-  cloud_watch_logs_role_arn = "${aws_iam_role.cloudwatch_logs_role.arn}"
-
-  event_selector {
-    read_write_type = "All"
-    include_management_events = false
-
-    data_resource {
-      type = "AWS::S3::Object"
-      values = [
-        "${aws_s3_bucket.cloudtrail_s3_logs.arn}/",
-        "${aws_s3_bucket.cloudtrail_management_logs.arn}/"
-      ]
-    }
-  }
-}
-
-resource "aws_cloudtrail" "management_logs" {
-  name = "management-logs"
-  s3_bucket_name = "${aws_s3_bucket.cloudtrail_management_logs.id}"
-  include_global_service_events = true
-  enable_log_file_validation = true
-
-  cloud_watch_logs_group_arn = "${aws_cloudwatch_log_group.management_logs.arn}"
-  cloud_watch_logs_role_arn = "${aws_iam_role.cloudwatch_logs_role.arn}"
-}
-
+// Logs for rap sheets S3 bucket access, s3 buckets deleted 1/14/2020
+// Retain logs for 1 year (~1/20/2021)
 resource "aws_s3_bucket" "cloudtrail_s3_logs" {
   bucket = "cloudtrail-s3-logs-${var.environment}"
   policy = <<POLICY
@@ -465,6 +248,35 @@ resource "aws_s3_bucket" "cloudtrail_s3_logs" {
     ]
 }
 POLICY
+}
+
+resource "aws_cloudwatch_log_group" "s3_logs" {
+  name = "s3_logs"
+}
+
+// Logs for accessing the S3 and management logs
+// Retain until account decommissioned
+resource "aws_cloudtrail" "log_access_logs" {
+  name = "log-access-logs"
+  s3_bucket_name = "${aws_s3_bucket.cloudtrail_log_access_logs.id}"
+  include_global_service_events = false
+  enable_log_file_validation = true
+
+  cloud_watch_logs_group_arn = "${aws_cloudwatch_log_group.log_access_logs.arn}"
+  cloud_watch_logs_role_arn = "${aws_iam_role.cloudwatch_logs_role.arn}"
+
+  event_selector {
+    read_write_type = "All"
+    include_management_events = false
+
+    data_resource {
+      type = "AWS::S3::Object"
+      values = [
+        "${aws_s3_bucket.cloudtrail_s3_logs.arn}/",
+        "${aws_s3_bucket.cloudtrail_management_logs.arn}/"
+      ]
+    }
+  }
 }
 
 resource "aws_s3_bucket" "cloudtrail_log_access_logs" {
@@ -515,6 +327,22 @@ resource "aws_s3_bucket" "cloudtrail_log_access_logs" {
 POLICY
 }
 
+resource "aws_cloudwatch_log_group" "log_access_logs" {
+  name = "log_access_logs"
+}
+
+// Logs for AWS management
+// Retain until account decommissioned
+resource "aws_cloudtrail" "management_logs" {
+  name = "management-logs"
+  s3_bucket_name = "${aws_s3_bucket.cloudtrail_management_logs.id}"
+  include_global_service_events = true
+  enable_log_file_validation = true
+
+  cloud_watch_logs_group_arn = "${aws_cloudwatch_log_group.management_logs.arn}"
+  cloud_watch_logs_role_arn = "${aws_iam_role.cloudwatch_logs_role.arn}"
+}
+
 resource "aws_s3_bucket" "cloudtrail_management_logs" {
   bucket = "cloudtrail-management-logs-${var.environment}"
   policy = <<POLICY
@@ -561,14 +389,6 @@ resource "aws_s3_bucket" "cloudtrail_management_logs" {
     ]
 }
 POLICY
-}
-
-resource "aws_cloudwatch_log_group" "s3_logs" {
-  name = "s3_logs"
-}
-
-resource "aws_cloudwatch_log_group" "log_access_logs" {
-  name = "log_access_logs"
 }
 
 resource "aws_cloudwatch_log_group" "management_logs" {
